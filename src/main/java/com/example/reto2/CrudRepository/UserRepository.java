501e24b20e44c7493298b406a196f223446c4b15
Juan Barragan <juan.barragan.mt@correo.usa.edu.co>
2021-12-12 20:52:07 -0500

    Reto03 Ciclo 4

---
+++ src/main/java/com/example/reto2/CrudRepository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.example.reto2.CrudRepository;
+
+// import java.util.List;
+import java.util.Optional;
+
+import com.example.reto2.Models.Documents.User;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+
+
+public interface UserRepository extends MongoRepository<User, Integer>{
+	
+	@Query("{email:?0}")
+	Optional<User> getEmail(String email);
+	
+	@Query("{email:?0, password:?1}")
+	Optional<User> getEmailAndPassword(String email, String pass);
+	
+	
+	
+	
+}
